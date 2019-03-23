76cc8d042eb80cc25434eb98736cb5bab0d65799
ricardovejarano10 <ricardo.vejarano.garcia@gmail.com>
2019-03-22 19:48:06 -0500

    Problem 1 done

--- reto_3_1/index.js
+++ reto_3_1/index.js
@@ -1,26 +1,20 @@
-var M;
-
-document.getElementById('average-calculated').style.visibility = 'hidden';
-document.getElementById('average-calculated').style.display = 'none';
-
 function calculate() {
     // First its evaluated if there are an empty entry
-    var note1 = document.getElementById('note1').value;
-    var note2 = document.getElementById('note2').value;
-    var note3 = document.getElementById('note3').value;
-    var note4 = document.getElementById('note4').value;
-    if (note1 && note2 && note3 && note4) {
-        if (note1 > 0 && note1 < 100 && note2 > 0 && note2 < 100 && note3 > 0 && note3 < 100 && note4 > 0 && note4 < 100 ) {
-            var average = (parseFloat(note1) + parseFloat(note2) + parseFloat(note3) + parseFloat(note4)) / 4;
-            document.getElementById('average-show').innerHTML = average;
-            document.getElementById('average-calculated').style.visibility = 'visible';
-            document.getElementById('average-calculated').style.display = 'block';
-        } else {
-            Materialize.toast('Enter a valid value', 4000);    
-        }
-    } else {
-        Materialize.toast('All notes are required', 4000);
+    var salary = document.getElementById('salary-entry').value;
+    salary = Number(salary);
+
+    if (salary > 0 && salary <= 9000) {
+        salary = salary + salary * 0.2;
+    } else if (salary > 9000 && salary <= 15000) {
+        salary = salary + salary * 0.1;
+    } else if (salary > 15000 && salary <= 20000) {
+        salary = salary + salary * 0.05;
+    } else if (salary > 20000) {
+        salary = salary;
     }
+
+    document.getElementById('salary').innerHTML = '<p>New Salary: ' + salary + ' </p>';;
+
 }
 
 
