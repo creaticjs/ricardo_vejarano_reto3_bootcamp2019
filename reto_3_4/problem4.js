11daa4825465b36eb170bcd82692322eb1d2e3fa
ricardovejarano10 <ricardo.vejarano.garcia@gmail.com>
2019-03-23 11:10:25 -0500

    Problem 4 done

--- reto_3_4/problem4.js
+++ reto_3_4/problem4.js
@@ -1,12 +1,33 @@
 function calculate() {
-    var s1 = document.getElementById('side1').value;
-    var s2 = document.getElementById('side2').value;
-    if (s1 && s2) {
-        s12 = Math.pow(s1, 2);
-        s22 = Math.pow(s2, 2);
-        hyp = Math.sqrt(s12 + s22);
-        document.getElementById('hypotenuse').innerHTML = '<p>Hypotenuse: ' + hyp + '</p>';
+    var year = document.getElementById('year-entry').value;
+    year = Number(year);
+    var a = 0;
+    var b = 0;
+    var c = 0;
+    var d = 0;
+    var n = 0;
+
+    if (year >= 1990 && year <= 2000) {
+
+        a = year % 19;
+        b = year % 4;
+        c = year % 7;
+        d = (19 * a + 24) % 30;
+        e = ((2 * b) + (4 * c) + (6 * d) + 5) % 7;
+        n = (22 + d + e);
+        printDate(n);
+
+
     } else {
-        Materialize.toast('All fields are required', 4000);
+        Materialize.toast('Invalid year', 4000);
+    }
+}
+
+function printDate(n) {
+    if (n <= 31) {
+        document.getElementById('date').innerHTML = '<p>Date of Easter Sunday: ' + n + ' of March</p>';
+    } else {
+        const aprilDate = n - 31;
+        document.getElementById('date').innerHTML = '<p>Date of Easter Sunday: ' + aprilDate + ' of April</p>';
     }
 }
