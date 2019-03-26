7dcd3e6321c5918562d76ade02b2756bb36ee5ce
ricardovejarano10 <ricardo.vejarano.garcia@gmail.com>
2019-03-26 12:45:11 -0500

    Problem 11 done

--- reto_3_11/problem11.js
+++ reto_3_11/problem11.js
@@ -1,35 +1,22 @@
-var input;
-var reader = new FileReader();
-
-function openFile(event) {
-    input = event.target;
-    reader = new FileReader();
-    reader.onload = function () {
-        var text = reader.result;
-        calculateN(text);
-    };
-    reader.readAsText(input.files[0]);
-};
-
 function calculate() {
-    input.files[0] = '';
-}
+    var number = document.getElementById('numberTyped').value;
+    number = Number(number);
+    isPrime = true;
+    var i = 2;
 
-
-function calculateN(text) {
-    var arrayNumber = text.split(',');
-    if (arrayNumber.length === 4) {
-        var secondNumber = Number(arrayNumber[2] + arrayNumber[3]);
-        var firstNumber = Number(arrayNumber[0] + arrayNumber[1] + '00');
-
-        if (secondNumber >= 50) {
-            firstNumber = firstNumber + 100;
-            document.getElementById('number').innerHTML = '<p>Result: ' + firstNumber + ' </p>';
-            console.log(firstNumber);
-        } else {
-            document.getElementById('number').innerHTML = '<p>Result: ' + firstNumber + ' </p>';
+    do {
+        if(number % i === 0) {
+            isPrime = false;
         }
+        i++;
+    } while(i < number);
+
+
+    if (isPrime) {
+        document.getElementById('isPrime').innerHTML = '<p>Is prime</p>';
     } else {
-        Materialize.toast('Invalid data', 4000);
+        document.getElementById('isPrime').innerHTML = '<p>Not prime </p>';
     }
 }
+
+
