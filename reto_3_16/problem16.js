b4146836a521002bb43b6ffa7974fd8e0d7ffc00
ricardovejarano10 <ricardo.vejarano.garcia@gmail.com>
2019-03-26 15:34:40 -0500

    Problem 16 done

--- reto_3_16/problem16.js
+++ reto_3_16/problem16.js
@@ -1,26 +1,32 @@
 function calculate() {
-    var price_hour = document.getElementById('price_hour').value;
-    var hours_worked = document.getElementById('hours_worked').value;
-    var salaryNoTax = 0;
-    var salary = 0;
-    price_hour = parseInt(price_hour);
-    hours_worked = parseInt(hours_worked);
-    if (hours_worked > 0 && hours_worked < 38) {
-        salaryNoTax = hours_worked * price_hour;
-        salary = calculateTaxes(salaryNoTax);
+    var number = document.getElementById('number').value;
+    number = Number(number);
+    var result = 0;
+    var arraySum = 0;
+    var i = 1;
+    var lastNumber = 0;
+    var flagMayor = false;
+
+    if (number > 0) {
+        while (!flagMayor) {
+            result = 0;
+            for (var x = 1; x < i; x++) {
+                result += x;
+
+                lastNumber = i;
+            }
+            if (result >= number) {
+                console.log(result, i);
+                flagMayor = true;
+            }
+            i++;
+        }
+
+        console.log('VALOR', lastNumber);
+
+        document.getElementById('result').innerHTML = '<p>Result: ' + (lastNumber -1) + ' </p>';
     } else {
-        var extraHours = hours_worked - 37;
-        var percent_50_up = price_hour + (price_hour * 0.5);
-        salaryNoTax = (37 * price_hour) + (extraHours * percent_50_up);
-        salary = calculateTaxes(salaryNoTax);
+        Materialize.toast('Invalid number', 4000);
     }
-    document.getElementById('salary').innerHTML = '<p>Salary: ' + salary + ' </p>';
 }
 
-function calculateTaxes(salaryNoTax) {
-    if (salaryNoTax > 50000) {
-        return salaryNoTax - (salaryNoTax * 0.1);
-    } else {
-        return salaryNoTax
-    }
-}
